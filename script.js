d3ec3a1cef277ed2c92f9565343e32e2e90ab571
alisbai <ali.sbai.96@gmail.com>
2022-03-11 07:31:20 +0100

    fixed the Email verification class
    css grid is now using max-content
    
    html input auto complete is disabled

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 class EmailVerification {
-    constructor(input, errorIcon) {
+    constructor(input, errorIcon, warning) {
         this.DOMInput = input;
         this.errorIcon = errorIcon;
+        this.warning = warning;
     }
     addEvent(eventType) {
-        this.DOMInput.addEventListener(eventType, this.verify);
-        console.log(this.DOMInput); // DOMElement is logged
+        this.DOMInput.addEventListener(eventType, () =>this.verify());
     }
 
     verify() {
-        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        console.log(this.DOMInput); // but it's undefined here :(
-        if(!emailRegex.test(this.DOMInput)) {
-            console.log(this.DOMInput);
+        let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        if(!emailRegex.test(this.DOMInput.value)) {
             this.errorIcon.classList.add('display-error-icon');
             this.DOMInput.classList.add('email-input-not-valid');
+            this.warning.classList.add('display-warning')
         }
         else {
             this.DOMInput.classList.remove('email-input-not-valid');
             this.errorIcon.classList.remove('display-error-icon');
+            this.warning.classList.remove('display-warning');
         }
     }
 }
 let DOMEmailInput = document.getElementById('email-input');
 let errorIcon = document.getElementById('error-icon');
-let emailInput = new EmailVerification(DOMEmailInput, errorIcon);
+let warning = document.getElementById('email-warning')
+let emailInput = new EmailVerification(DOMEmailInput, errorIcon, warning);
 emailInput.addEvent('input');
